044d531bf90bfc0eaeadcba5f0c3f01d30d81533
jvalencial01 <150673634+jvalencial01@users.noreply.github.com>
2023-11-13 12:28:00 +0100

    Add files via upload

---
+++ Procesosporlote/mkuserprofile.bat.bat
@@ -0,0 +1,16 @@
+@echo off
+REM scrip que genera directorios
+REM
+REM auto: Javier Valencia Larios
+REM fecha: 13-11-2023
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti
+
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam
+
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\exam
